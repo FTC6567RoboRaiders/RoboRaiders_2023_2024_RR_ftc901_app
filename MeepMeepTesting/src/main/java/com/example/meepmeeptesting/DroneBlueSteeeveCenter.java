6298f91ce726874857fd27d22fd88e2034e9a03f
skocik <skocik@us.ibm.com>
2023-12-12 15:43:46 -0500

    Added a few classes for the drone blue side to show on possible path

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/DroneBlueSteeeveCenter.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/DroneBlueSteeeveCenter.java
@@ -1,27 +1,28 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-//import java.util.Vector;
-
-public class DroneBlue {
+public class DroneBlueSteeeveCenter {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setDimensions(13,17)    // Set the dimensions of the robot
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(-35, 60, Math.toRadians(270)))
-                                .forward(37)
-                                .back(10)
-                                .splineTo(new Vector2d(-31, 34),Math.toRadians(180))
-                                .splineTo(new Vector2d(-24, 35),Math.toRadians(180))
-                                .back(40)
+                                .forward(25)
+//   --------------------------------------------------------------------------------------------
+//   place the purple pixel after forward
+//   --------------------------------------------------------------------------------------------
+                                .turn(Math.toRadians(-90))
+//                                .splineTo(new Vector2d(-31, 34),Math.toRadians(180))
+//                                .splineTo(new Vector2d(-24, 35),Math.toRadians(180))
+                                .back(82)
 
 //                                .turn(Math.toRadians(90))
 //                                .forward(30)
