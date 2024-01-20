4263dbeaeafe948be583e297fb113666294ea64d
Roboraiders164 <roboraiders164@gmail.com>
2024-01-20 10:37:02 -0500

    added vision-related stuff

--- TeamCode/src/main/java/RoboRaiders/Auto/TournAuto.java
+++ TeamCode/src/main/java/RoboRaiders/Auto/TournAuto.java
@@ -23,6 +23,8 @@ public class TournAuto extends LinearOpMode {
     public AprilTagDetection desiredTag = null;
     public StevesPipeline2 stevesPipeline;
     public int position;
+    public long startTime;
+    public long elapsedTime;
 
     private static final boolean USE_WEBCAM = true;
 
@@ -38,56 +40,44 @@ public class TournAuto extends LinearOpMode {
 
         gamepad1.reset();
 
-        initVision();
+        initColourPortal();
 
         position = propPosition();
 
+        waitForStart();
+
+        elapsedTime = System.nanoTime() - startTime;
+
+        // spike mark positions
         switch (position) {
+            // move to left spikemark
             case 0:
-                // move to left spikemark
                 break;
+            // move to centre spikemark
             case 1:
-                // move to centre spikemark
                 break;
+            // move to right spikemark
             case 2:
-                // move to right spikemark
                 break;
             default:
                 break;
         }
 
-        waitForStart();
+        initAprilTagPortal();
+
+        // intake and deposit loop
+        while((elapsedTime / 1000000000) >= 5) {
+
+
+
+        }
 
     }
 
-//    public void initAprilTag() {
-//        // Create the AprilTag processor by using a builder.
-//        aprilTag = new AprilTagProcessor.Builder().build();
-//
-//        // Adjust Image Decimation to trade-off detection-range for detection-rate.
-//        // eg: Some typical detection data using a Logitech C920 WebCam
-//        // Decimation = 1 ..  Detect 2" Tag from 10 feet away at 10 Frames per second
-//        // Decimation = 2 ..  Detect 2" Tag from 6  feet away at 22 Frames per second
-//        // Decimation = 3 ..  Detect 2" Tag from 4  feet away at 30 Frames Per Second
-//        // Decimation = 3 ..  Detect 5" Tag from 10 feet away at 30 Frames Per Second
-//        // Note: Decimation can be changed on-the-fly to adapt during a match.
-//        aprilTag.setDecimation(2);
-//
-//        // Create the vision portal by using a builder.
-//        if (USE_WEBCAM) {
-//            visionPortal = new VisionPortal.Builder()
-//                    .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
-//                    .addProcessor(aprilTag)
-//                    .build();
-//        } else {
-//            visionPortal = new VisionPortal.Builder()
-//                    .setCamera(BuiltinCameraDirection.BACK)
-//                    .addProcessor(aprilTag)
-//                    .build();
-//        }
-//    }
 
-    public void initVision() {
+
+    public void initAprilTagPortal() {
+
         // Create the AprilTag processor by using a builder.
         aprilTag = new AprilTagProcessor.Builder().build();
 
@@ -112,7 +102,9 @@ public class TournAuto extends LinearOpMode {
                     .setCamera(BuiltinCameraDirection.BACK)
                     .addProcessor(aprilTag)
                     .build();
+
         }
+
     }
 
     public int propPosition() {
@@ -125,8 +117,7 @@ public class TournAuto extends LinearOpMode {
         else {
             return 0;
         }
+
     }
-
-
 
 }
---
+++ TeamCode/src/main/java/RoboRaiders/TeleOp/PirsusTeleop.java
@@ -0,0 +1,41 @@
+package RoboRaiders.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import RoboRaiders.Robots.PirsusMkII;
+import RoboRaiders.Utilities.Logger.Logger;
+
+@TeleOp (name = "Pirsus TeleOp")
+public class PirsusTeleop extends OpMode {
+
+    public PirsusMkII robot = new PirsusMkII();
+    public Logger myLogger =  new Logger("TestBotTeleop");
+    public Logger dtLogger = new Logger("DT");   // Drive train logger
+
+    public long startTime;
+    public long elapsedTime;
+    public boolean endGame = false;
+
+    @Override
+    public void init() {
+        // initialise robot and tell user that the robot is initialised
+        robot.initialize(hardwareMap);
+        telemetry.addData("Robot Initialized waiting your command", true);
+        telemetry.update();
+    }
+
+    @Override
+    public void start() {
+        //Timer for drone launch safety
+        startTime = System.nanoTime();
+    }
+
+    @Override
+    public void loop() {
+
+
+
+    }
+
+}
