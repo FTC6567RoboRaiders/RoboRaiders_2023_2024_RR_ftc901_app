9d1318a50646f7a509d76df4757993ec7d6371c3
Roboraiders164 <roboraiders164@gmail.com>
2024-02-01 16:58:07 -0500

    PirsusPath is a 2+4 auto

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/PirsusPath.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/PirsusPath.java
@@ -5,6 +5,7 @@ import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import java.util.Random;
 
 public class PirsusPath {
     public static void main(String[] args) {
@@ -22,17 +23,32 @@ public class PirsusPath {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .setDimensions(13,17)    // Set the dimensions of the robot
                 .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-35, 60, Math.toRadians(90)))
-                                .back(48.5) // move off wall
+                                .back(40) // move off wall
                                 .waitSeconds(2) // placeholder for dropping purple
                                 .back(1) // fixes spline going forward issue
                                 .splineTo(new Vector2d(0, 11.5), Math.toRadians(0)) // spline to door
                                 .back(24) // drive to stage
                                 .splineToConstantHeading(new Vector2d(47, 35), Math.toRadians(0)) // spline to stage
-                                .waitSeconds(4)
-                                // end cupcake auto
+                                .waitSeconds(3) // placeholder for dropping yellow
+                                // end initial cycle
 
-                                .splineToConstantHeading(new Vector2d(18, 11.5), Math.toRadians(0)) // spline to pixels
-                                .forward(75)
+                                // begin cycle 2
+                                .lineToConstantHeading(new Vector2d(26, 11.5))
+                                .forward(84) // move to pixel
+                                .waitSeconds(1) // placeholder for collecting pixels
+                                .back(82) // move to stage
+                                .splineToConstantHeading(new Vector2d(47, 35), Math.toRadians(0)) // spline to stage
+                                .waitSeconds(3) // placeholder for dropping pixels
+                                // end cycle 2
+
+                                // begin cycle 3
+                                .lineToConstantHeading(new Vector2d(26, 11.5))
+                                .forward(84) // move to pixel
+                                .waitSeconds(1) // placeholder for collecting pixels
+                                .back(82) // move to stage
+                                .splineToConstantHeading(new Vector2d(47, 35), Math.toRadians(0)) // spline to stage
+                                .waitSeconds(3) // placeholder for dropping pixels
+                                // end cycle 3
 
 //                                .turn(Math.toRadians(90))
 //                                .forward(30)
